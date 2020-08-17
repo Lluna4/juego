9962f9b439ba7fb0dc77df2904c262b14a56934a
calyjb17 <69774917+calyjb17@users.noreply.github.com>
2020-08-17 17:06:50 +0200

    Add files via upload
    Bug fixes and crosshair addition

--- arma.gd
+++ arma.gd
@@ -4,11 +4,19 @@ onready var raycast = $"../cabeza/Camera/RayCast"
 export var velocidad_disparo = 0.5
 export var balas_cargador = 20
 export var tiempo_recarga = 1
+export var respawn_s = 3
 onready var balas_ui = $"/root/mundo/algo/balas"
+onready var enemigo_cargado = $"/root/mundo/enemigo"
 var balas = balas_cargador
+
 var se_puede_disparar = true
 var recargando = false
+
+var rng = RandomNumberGenerator.new()
 var balas_ui_recursos = [balas, balas_cargador]
+
+
+
 func _process(delta: float) -> void:
 	if recargando:
 		balas_ui.set_text("Recargando!")
@@ -17,25 +25,40 @@ func _process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("disparo") and se_puede_disparar:
 		if balas > 0 and not recargando:
-			print("BANG!")
-			se_puede_disparar = false      #IF para disparar
-			balas -= 1
-			revisar_colision()
-			yield(get_tree().create_timer(velocidad_disparo), "timeout")
-			se_puede_disparar = true
+			disparo()
 		elif not recargando:
-			print("Recargando")
-			recargando = true         #IF para recargar
-			yield(get_tree().create_timer(tiempo_recarga), "timeout")
-			recargando = false
-			balas = balas_cargador
-			print("Recarga completada")
-		  
+			recarga()
 		
-		
+	if Input.is_action_pressed("recarga") and not recargando:
+		recarga()
+	
 func revisar_colision():
+	 
 	if raycast.is_colliding():
 		var collider = raycast.get_collider()
 		
 		collider.queue_free()
 		print("MATADO " + collider.name)
+		yield(get_tree().create_timer(tiempo_recarga), "timeout")
+		var enemigo = preload("res://enemigo.tscn").instance()
+		rng.randomize()
+		var my_random_number = rng.randf_range(-25, 25)
+		enemigo.translation = Vector3 (my_random_number, 0, my_random_number)
+		add_child(enemigo)
+
+func disparo():
+	print("BANG!")
+	se_puede_disparar = false     
+	balas -= 1
+	revisar_colision()
+	yield(get_tree().create_timer(velocidad_disparo), "timeout")
+	se_puede_disparar = true
+	
+
+func recarga():
+	print("Recargando")
+	recargando = true         
+	yield(get_tree().create_timer(tiempo_recarga), "timeout")
+	recargando = false
+	balas = balas_cargador
+	print("Recarga completada")
